751068f017d6c55ae1af0daf8f429b07f773656c
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2024-04-07 23:01:05 +0900

    Adding new cafe part has been finished

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 import csv
 import pandas
 
@@ -13,6 +13,13 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    maps = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    opening = StringField('Opening Time e.g.8A.M', validators=[DataRequired()])
+    closing = StringField('Closing time e.g.5P.M', validators=[DataRequired()])
+    coffee_rating = SelectField("Coffee Rating", choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
+    wifi_rating = SelectField("Wifi Strength Rating", choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_rating = SelectField("Power Socket Availability", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    
     submit = SubmitField('Submit')
 
 # Exercise:
@@ -30,14 +37,27 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=["POST", "GET"])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        cafe_name = form.cafe.data
+        map_link = form.maps.data
+        opening = form.opening.data
+        closing = form.closing.data
+        coffee_rating = form.coffee_rating.data
+        wifi_rating = form.wifi_rating.data
+        power_rating = form.power_rating.data
+
+        with open('cafe-data.csv', mode='a', encoding='utf-8') as file:
+            file.write(f"\n{cafe_name},"
+                       f"{map_link},"
+                       f"{opening},"
+                       f"{closing},"
+                       f"{coffee_rating},"
+                       f"{wifi_rating},"
+                       f"{power_rating}")
+        return redirect('/cafes')
     return render_template('add.html', form=form)
 
 
